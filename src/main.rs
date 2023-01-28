41de8ef8cd3f408bd6aa03d10f57751b99360815
SoonIter <sooniter@gmail.com>
2023-01-29 00:08:09 +0800

    🔖 v0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use picocolors_rs::bg_black;
+use picocolors_rs::{bg_black, bold};
 fn main() {
-    println!("hello {}", bg_black(format!("123123 {}", "1111").as_str()));
+    println!(
+        "hello {}",
+        bold(&bg_black(format!("123123 {}", "1111").as_str()))
+    );
 }
