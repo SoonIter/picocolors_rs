e87178ea54131063fdd1d0838ffa7f2e3900cc9f
SoonIter <sooniter@gmail.com>
2024-01-12 19:28:50 +0800

    chore: rename  picocolors-rs => picocolors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use picocolors_rs::{bg_black, bold};
+use picocolors::{bg_black, bold, formatter};
 fn main() {
   println!("hello {}", bold(bg_black("world")));
+
+  let my_custom_blue = formatter("\x1b[0m", "\x1b[0m");
+  println!("hello {:?}", my_custom_blue("world"));
 }
