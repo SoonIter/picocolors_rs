ad0d52b06d457f2262c0f2ad5dd6a45707147368
SoonIter <sooniter@gmail.com>
2024-01-29 19:10:14 +0800

    chore: some words polish

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use picocolors::{bg_black, bold, formatter};
 fn main() {
   println!("hello {}", bold(bg_black("world")));
 
-  let my_custom_blue = formatter("\x1b[0m", "\x1b[0m");
-  println!("hello {:?}", my_custom_blue("world"));
+  let my_custom_blue = formatter("\x1b[34m", "\x1b[39m");
+  println!("hello {}", my_custom_blue("world"));
 }
